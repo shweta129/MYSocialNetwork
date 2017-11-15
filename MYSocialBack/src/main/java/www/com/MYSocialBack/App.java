2ffdd2325cc5a8458af26784ec20089d230b7587
shweta <shwetachaudhari129@gmail.com>
2017-11-15 13:35:07 +0530

    adding dependencies in POM

---
+++ MYSocialBack/src/main/java/www/com/MYSocialBack/App.java
@@ -0,0 +1,13 @@
+package www.com.MYSocialBack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
