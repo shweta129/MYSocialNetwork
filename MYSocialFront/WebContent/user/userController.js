c59f527affaa1c6874b149920e99edb274538fb3
shweta <shwetachaudhari129@gmail.com>
2017-11-16 12:18:41 +0530

    fetch user data from backend in UI

--- MYSocialFront/WebContent/user/userController.js
+++ MYSocialFront/WebContent/user/userController.js
@@ -4,8 +4,21 @@ app.controller('loginController', function($scope) {
 
 app.controller('registrationController', function($scope,userService) {
 	$scope.message = 'Look! I am an registration page.';
-		
-	$scope.user={id:'',first_name:'',last_name:'',password:'',email:'',Number:'',address:'',dob:'',successMessage:'',errorMesssage:''};
+	
+	userService.getAllCountry().then(function(response){
+		$scope.countries=response.data
+		console.log("Country List::"+$scope.countries);
+		})
+	userService.getAllUser().then(function(response){
+		$scope.users=response.data
+		console.log("User List::"+$scope.users);
+		})
+});
+
+	
+	
+	
+	/*$scope.user={id:'',first_name:'',last_name:'',password:'',email:'',Number:'',address:'',dob:'',successMessage:'',errorMesssage:''};
 	
 	$scope.register= function(){
 		
@@ -21,10 +34,5 @@ app.controller('registrationController', function($scope,userService) {
 		
 		
 	}
+	*/
 	
-	userService.getAllUser().then(function(response){
-		$scope.users=response.data
-		console.log("User List::"+$scope.users);
-		})
-	
-});
--- MYSocialFront/WebContent/user/userServices.js
+++ MYSocialFront/WebContent/user/userServices.js
@@ -1,16 +1,19 @@
 app.factory("userService",function($http){
     var BASE_URL="http://localhost:7070/MYSocialRest";
-var userService ={};
+	var userService ={};
+	userService.getAllCountry=function(){
+		return $http.get(BASE_URL+"/hello/AllCountry");		
+	}
+	
 	
 	
 	userService.getAllUser=function(){
 		return $http.get(BASE_URL+"/user/getAllUsers");		
 	}
-	
-	userService.createUser=function(user){
-		console.log("****** inside user service--createUser()*****")
-		return $http.post(BASE_URL+"/user/register",user);		
-	}
-	
 	return userService;
 });
+	/*userService.createUser=function(user){
+		console.log("****** inside user service--createUser()*****")
+		return $http.post(BASE_URL+"/user/register",user);		
+	}*/
+	
